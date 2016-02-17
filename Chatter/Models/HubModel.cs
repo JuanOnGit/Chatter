e7c8799cc874f87f260a0fbc3b6597ba7a031832
Pito289 <Juan.M.Davila.Jr@hotmail.com>
2016-02-17 09:05:10 -0600

    Initial Commit completed.

---
+++ Chatter/Models/HubModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chatter.Models
+{
+    public class HubModel
+    {
+        public string HubName { get; set; }
+        [Key]
+        public int HubID { get; set; }
+        public int UserCount { get; set; }
+    }
+}
---
+++ Chatter/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Chatter;
+
+[assembly: OwinStartupAttribute(typeof(Chatter.Startup))]
+namespace Chatter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            app.MapSignalR();
+        }
+    }
+}
